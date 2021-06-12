fbc8ff6bdc77f10a4ee0fc414ad8f09473b6be5d
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-06-12 23:20:41 +0530

    update settings and urls

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -99,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = "accounts.Account"
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
