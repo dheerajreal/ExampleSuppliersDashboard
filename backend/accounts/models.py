7d5eef142f016062ef1c6584fb10cd33b72faa0a
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-06-12 23:21:19 +0530

    add account models and tests

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,107 @@
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.core.mail import send_mail
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model"""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+    def create_supplier(self, email, password, **extra_fields):
+        """Create and save a Supplier with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_supplier', True)
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class AccountBase(AbstractBaseUser, PermissionsMixin):
+    """Abstract model for Accounts"""
+    business_name = models.CharField(max_length=150, blank=True, null=True)
+    business_address = models.TextField(blank=True, null=True)
+    email = models.EmailField(blank=True, unique=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    is_supplier = models.BooleanField(
+        _('supplier'),
+        default=False,
+        help_text=_(
+            'Supplier Account. '
+            'Account for suppliers.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('account')
+        verbose_name_plural = _('accounts')
+        abstract = True
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_name(self):
+        """Return the short name for the user."""
+        return self.business_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+
+class Account(AccountBase):
+    """Model for Accounts"""
+    class Meta(AccountBase.Meta):
+        swappable = 'AUTH_USER_MODEL'
--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,51 @@
+from accounts.models import Account
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestAccounts(TestCase):
+    def test_users_in_db_count(self):
+        self.account1 = Account.objects.create_user(
+            email="account1@example.com", password="abc")
+        self.supplier1 = Account.objects.create_supplier(
+            email="supplier1@example.com", password="abc")
+        self.admin = Account.objects.create_superuser(
+            email="admin1@example.com", password="abc")
+
+        users = Account.objects.all()
+        self.assertEqual(users.count(), 3)
+
+    def test_create_supplier(self):
+        email = "supplier@example.com"
+        password = "password"
+        business_name = "abc"
+        business_address = "abc street"
+        supplier = Account.objects.create_supplier(
+            email=email,
+            password=password,
+            business_name=business_name,
+            business_address=business_address
+        )
+        self.assertEqual(supplier.is_supplier, True)
+        self.assertEqual(supplier.is_staff, False)
+        self.assertEqual(supplier.business_name, business_name)
+        self.assertEqual(supplier.business_address, business_address)
+        self.assertEqual(
+            Account.objects.get(email=email),
+            supplier
+        )
+
+    def test_create_admin(self):
+        email = "admin@example.com"
+        password = "password"
+        admin = Account.objects.create_superuser(
+            email=email,
+            password=password,
+        )
+        self.assertEqual(admin.is_supplier, False)
+        self.assertEqual(admin.is_superuser, True)
+        self.assertIsNone(admin.business_name)
+        self.assertIsNone(admin.business_address)
+        self.assertEqual(
+            Account.objects.get(email=email),
+            admin
+        )
