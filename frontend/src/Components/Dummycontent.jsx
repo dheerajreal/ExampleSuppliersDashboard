331da8b5763a8b83abd5e50eab0eeb255eb5c93c
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-06-13 21:03:57 +0530

    add dummycontent

---
+++ frontend/src/Components/Dummycontent.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Dummycontent = () => {
+  return (
+    <div className="container mt-2">
+      <div className="row">
+        <div className="col-1">
+          <Link  >
+            <p>dummy</p>
+          </Link>
+        </div>
+        <div className="col-1">
+          <Link  >
+            <p>dummy</p>
+          </Link>
+        </div>
+        <div className="col-1">
+          <Link  >
+            <p>dummy</p>
+          </Link>
+        </div>
+        <div className="col-1">
+          <Link  >
+            <p>dummy</p>
+          </Link>
+        </div>
+        <div className="col-1">
+          <Link  >
+            <p>dummy</p>
+          </Link>
+        </div>
+        <div className="col-1">
+          <Link  >
+            <p>dummy</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dummycontent;
