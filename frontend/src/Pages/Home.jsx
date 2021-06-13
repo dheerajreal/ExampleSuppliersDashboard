8c2f27f500a12c9e6e348bb44af67f41ff7a3de6
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-06-13 15:19:17 +0530

    add admin and profile page

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { Redirect } from "react-router";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+const Home = () => {
+  const history = useHistory();
+
+  const refresh_token = localStorage.getItem("refresh_token");
+  console.log(refresh_token);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/myaccount", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      })
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("is_supplier", data.data.is_supplier);
+        localStorage.setItem("is_staff", data.data.is_staff);
+        localStorage.setItem("email", data.data.email);
+        localStorage.setItem("business_name", data.data.business_name);
+        localStorage.setItem("business_address", data.data.business_address);
+      })
+      .then(() => {
+        let is_staff = localStorage.getItem("is_staff");
+        console.log(is_staff);
+        is_staff = String(is_staff).toLowerCase() === "true";
+        console.log(is_staff);
+
+        if (is_staff) {
+          history.push("/admin");
+        } else {
+          history.push("/profile");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        history.push("/login");
+      });
+  }, [history, refresh_token]);
+
+  if (!refresh_token) {
+    return <Redirect to="/login" />;
+  } else {
+    return <h1>checking your account</h1>;
+  }
+
+
+};
+
+export default Home;
---
+++ frontend/src/Pages/Profile.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { Redirect, useHistory } from "react-router-dom";
+
+const Profile = () => {
+  const refresh_token = localStorage.getItem("refresh_token");
+
+  const history = useHistory();
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/myaccount", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      })
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("is_supplier", data.data.is_supplier);
+        localStorage.setItem("is_staff", data.data.is_staff);
+        localStorage.setItem("email", data.data.email);
+        localStorage.setItem("business_name", data.data.business_name);
+        localStorage.setItem("business_address", data.data.business_address);
+      })
+      .then(() => {
+        let is_staff = localStorage.getItem("is_staff");
+        console.log(is_staff);
+        is_staff = String(is_staff).toLowerCase() === "true";
+        console.log(is_staff);
+
+        if (is_staff) {
+          history.push("/admin");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        history.push("/login");
+      });
+  }, [history]);
+
+  if (!refresh_token) {
+    return <Redirect to="/login" />;
+  } else {
+    return <h1>hi supplier</h1>;
+  }
+};
+
+export default Profile;
