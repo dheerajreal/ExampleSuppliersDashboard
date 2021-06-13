7e56c6ff25755ba853ea6938db6d444e3511d7b3
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-06-13 21:55:16 +0530

    swagger docs added

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "accounts",
     "rest_framework",
     "corsheaders",
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Supplier management Example API",
+        default_version='v1',
+        description="Supplier management Example ",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@demo.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('', include("accounts.urls")),
@@ -25,4 +42,23 @@ urlpatterns += [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls')),
+]
+
+
+urlpatterns += [
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
+    re_path(
+        r'^swagger/$',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    re_path(
+        r'^redoc/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
 ]
