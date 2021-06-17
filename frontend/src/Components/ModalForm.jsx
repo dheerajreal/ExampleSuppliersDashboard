1de88959c72e7d1f5425f0c211b2b79941739e8d
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-06-17 23:25:17 +0530

    edit accounts

--- frontend/src/Components/ModalForm.jsx
+++ frontend/src/Components/ModalForm.jsx
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Modal from "react-bootstrap/Modal";
+import { toast } from "react-toastify";
+import url from "../Utils/url";
 
 const ModalForm = ({ item }) => {
   const [name, setName] = useState("");
@@ -10,12 +13,20 @@ const ModalForm = ({ item }) => {
 
   useEffect(() => {
     // console.log(item);
-    setName(item.business_name);
-    setAddress(item.business_address);
-    setEmail(item.email);
-    setVisible(item.visible);
-    setPk(item.pk);
+    setName(item.business_name || "");
+    setAddress(item.business_address || "");
+    setEmail(item.email || "");
+    setVisible(item.visible || false);
+    setPk(item.pk || "");
   }, [item]);
+
+  const hideForm = (e) => {
+    setVisible(false);
+    setName("");
+    setAddress("");
+    setEmail("");
+    setPk("");
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,14 +40,31 @@ const ModalForm = ({ item }) => {
         pk: pk,
       };
       console.log(formdata);
-      //   axios.post();
+      axios
+        .put(
+          url + `/accounts/${pk}/`,
+          { ...formdata },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            },
+          }
+        )
+        .then((resp) => resp.data)
+        .then((data) => {
+          console.log(data);
+          toast.success("successfully updated");
+          hideForm();
+        })
+        .catch((err) => {
+          console.log(JSON.stringify(err));
+          toast.error(`An Error occurred`);
+        });
     }
   };
   return (
     <>
-      <Modal
-        show={visible}
-      >
+      <Modal show={visible}>
         <div className="mx-3">
           <form onSubmit={handleSubmit} noValidate className="needs-validation">
             <div className="row">
@@ -96,13 +124,7 @@ const ModalForm = ({ item }) => {
               Submit
             </button>
             <button
-              onClick={(e) => {
-                setVisible(false);
-                setName("");
-                setAddress("");
-                setEmail("");
-                setPk("");
-              }}
+              onClick={hideForm}
               className="mb-4 mt-4 btn btn-primary"
               style={{ marginLeft: 30 }}
             >
